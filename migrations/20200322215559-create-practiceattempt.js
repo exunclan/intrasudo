567afd30398efdd1a85948517bb1e2999b90595f
Kavin Valli <mail@kavin.me>
2022-06-14 20:59:47 +0530

    fix: attempts table

--- migrations/20200322215559-create-practiceattempt.js
+++ migrations/20200322215559-create-practiceattempt.js
@@ -6,40 +6,40 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       attempt: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       UserId: {
         type: Sequelize.INTEGER,
         references: {
           model: "Users",
-          key: "id"
+          key: "id",
         },
         onUpdate: "CASCADE",
-        onDelete: "CASCADE"
+        onDelete: "CASCADE",
       },
       LevelId: {
         type: Sequelize.INTEGER,
         references: {
           model: "Levels",
-          key: "id"
+          key: "id",
         },
         onUpdate: "CASCADE",
-        onDelete: "CASCADE"
+        onDelete: "CASCADE",
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable("Attempts");
-  }
+  },
 };
